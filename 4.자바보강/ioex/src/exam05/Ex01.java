1b45240bd59050dc2007267d3a145f415babda0e
Yeji-Yoon <yoonyeji12@naver.com>
2023-12-28 17:34:45 +0900

    파일 올리기

--- 4.자바보강/ioex/src/exam05/Ex01.java
+++ 4.자바보강/ioex/src/exam05/Ex01.java
@@ -4,18 +4,20 @@ import java.io.File;
 import java.io.IOException;
 
 public class Ex01 {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
         //File file = new File("D:/test1.txt");
         //file.createNewFile();
         //File dir = new File("D:/folder");
         File dir = new File("D:/folder1/folder2/folder3");
-        if(!dir.exists()){//파일또는 디렉토리 존재 유무
-            dir.mkdir();
+        if (!dir.exists()) { // 파일 또는 디렉토리 존재 유무
+            dir.mkdirs();
         }
-        File file = new File("D:/folder/text1.txt");
+        File file = new File(dir, "test1.txt");
         //file.createNewFile();
+        File file2 = File.createTempFile("tmp_", ".log", new File("D:/"));
 
-        File.createTempFile("tmp_",".log",new File("D:/"))
+        Thread.sleep(5000);
 
+        file2.deleteOnExit();
     }
 }
