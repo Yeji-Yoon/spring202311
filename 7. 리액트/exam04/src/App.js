983bd4314a87292d33dedd3b30cddd685d921fdd
Yeji-Yoon <yoonyeji12@naver.com>
2024-02-15 14:21:13 +0900

    리액트

--- 7. 리액트/exam04/src/App.js
+++ 7. 리액트/exam04/src/App.js
@@ -1,14 +1,17 @@
 import { Routes, Route } from 'react-router-dom';
-import About from './pages/About';
 import Home from './pages/Home';
-import profile from './pages/profile';
+import About from './pages/About';
+import Profile from './pages/Profile';
+import ProfileHeader from './pages/ProfileHeader';
+import MainLayout from './layouts/MainLayout';
 
 const App = () => {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/about" element={<About />} />
-      <Route path="/profile/username" element={<profile />} />
+      <Route path="/" element={<MainLayout />}>
+        <Route index element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Route>
     </Routes>
   );
 };
